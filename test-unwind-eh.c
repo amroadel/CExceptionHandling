1ec794b6d1a2addcb9e212a137e5d3d3db75e66c
amroadel <amroadel@aucegypt.edu>
2020-06-18 22:35:35 +0200

    Added: test_uw_frob_return_addr()

--- test-unwind-eh.c
+++ test-unwind-eh.c
@@ -385,159 +385,6 @@ init_context(struct test_Unwind_Context *context, void *outer_cfa, void *outer_r
     /* Special Handling: aarch64 needs modification for return address value */
 }
 
-void
-add_lsda(const unsigned char *fde, struct test_Unwind_Context *context)
-{
-    unsigned char lsda_encoding;
-    unsigned char fde_encoding;
-    void *lsda;
-    const unsigned char *cie; 
-    int fde_legnth  = *fde;
-    int cie_legnth; 
-    int cie_offset_value;
-    unsigned char cie_version;
-    const unsigned char *cie_aug;
-    const unsigned char *fde_aug;
-    const unsigned char *p; 
-    void *pc_begin;
-    _uleb128_t utmp;
-    _sleb128_t stmp;
-    int cie_id_offset;
-    int fde_id_offset; 
-    char z_flag = 0;  
-    
-
-    if(fde_legnth == 0xffffffff)
-        fde_id_offset =  12; //Legnth section + extended legnth section
-    else 
-        fde_id_offset = 4;
-
-    cie_offset_value = *(fde + fde_id_offset); // the byte offset to the start of the CIE with which this FDE is associated   
-    cie = (fde+fde_id_offset) - cie_offset_value; //the start of the cie (length record)
-    pc_begin = (void *)(fde + fde_id_offset + 4); //The starting address to which this FDE applies. 
-    //context->bases.func = pc_begin;
-
-    //TODO: if cie_legnth is 0, CIE shall be considered a terminator and the proccesing shall end
-    cie_legnth = *cie;
-    printf("cie legnth: %i\n", cie_legnth);
-    if(cie_legnth == 0xffffffff)
-        cie_id_offset =  12; //Legnth section + extended legnth section
-    else 
-        cie_id_offset = 4;
-    
-    cie_version = *(cie + cie_id_offset + 4); // cie_version is either 1 or 3
-    cie_aug = cie + cie_id_offset + 5;
-    printf("cie_version %u\n", cie_version); 
-    p = cie_aug + strlen ((const char *)cie_aug) + 1; // Skip the augmentation string.
-
-    /* g++ v2 "eh" has pointer immediately following augmentation string,
-       so it must be handled first.  */   
-    if (cie_aug[0] == 'e' && cie_aug[1] == 'h')
-    {
-      p += sizeof (void *);
-      cie_aug += 2;
-    }
-
-    p = read_uleb128 (p, &utmp);    /* Skip code alignment.  */
-    p = read_sleb128 (p, &stmp);    /* Skip data alignment.  */
-
-
-    if (cie_version == 1)
-        p++;
-    else
-        p = read_uleb128 (p, &utmp);
-    lsda_encoding = DW_EH_PE_omit;
-    
-
-   /* unsigned char aug_arr[6]; //hard codded for now (6 is the number of possible letters)
-    const unsigned char *cie_aug_p = cie_aug; //auxillary pointer to the start of the cie_aug section 
-    int i = 0;
-
-    while (*cie_aug_p != '\0')
-    {
-      aug_arr[i] = *cie_aug_p;
-      cie_aug_p++;
-      i++;
-    }
-    aug_arr[i] = '\0';
-    cie_aug = aug_arr;
-    int j = 0; 
-    for (j; j <= i; j++)
-        printf("value %i: %u\n", j, aug_arr[j]); */
-
-    
-    /* If the augmentation starts with 'z', then a uleb128 immediately
-     follows containing the length of the augmentation field following
-     the size.  */
-    if (*cie_aug == 'z')
-    {
-      p = read_uleb128 (p, &utmp);  /* Skip augmentation length.  */
-      ++cie_aug;
-      z_flag = 1;
-    }
-
-    while (*cie_aug != '\0')
-    {
-
-       /* "L" indicates a byte showing how the LSDA pointer is encoded.  */
-      if (cie_aug[0] == 'L')
-      {
-        printf("L\n");
-        lsda_encoding = *p++;
-        printf("lsda_encoding %u \n",lsda_encoding);
-        cie_aug += 1;      
-      }
-      /* "R" indicates a byte indicating how FDE addresses are encoded.  */
-      else if (cie_aug[0] == 'R')
-      {
-        printf("R\n");
-        fde_encoding = *p++;
-        cie_aug += 1;
-      }
-      else if (cie_aug[0] == 'P')
-      {
-        printf("P\n");
-        test_Unwind_Ptr personality; 
-        //p += sizeof (void *);
-        //p = read_encoded_value_with_base (*p , 0 , p ,&personality);
-        p = read_encoded_value_with_base (*p & 0x7F, 0, p + 1, &personality);
-        cie_aug += 1;
-      }
-      else 
-      {
-          printf("here elseee \n");
-          cie_aug +=1; 
-      }
-    } 
-    
-    fde_aug = fde + fde_id_offset + 4; //skip legnth and ID sections
-    //fde_aug = fde + sizeof (*fde);
-    printf("fde_aug1 %p \n",fde_aug);
-    fde_aug += 2 * size_of_encoded_value (fde_encoding);
-     printf("fde_aug2 %p \n",fde_aug);
-    
-    if (z_flag)
-    {
-      _uleb128_t i;
-      fde_aug = read_uleb128 (fde_aug, &i);
-      printf("fde_aug3 %p \n",fde_aug);
-    }
-    
-    if (lsda_encoding != DW_EH_PE_omit)
-    {
-      test_Unwind_Ptr lsda;
-       
-      test_Unwind_Ptr base = (test_Unwind_Ptr) header.eh_frame;
-      printf("base %p \n", (void *)base);
-      fde_aug = read_encoded_value_with_base(lsda_encoding, base, fde_aug, &lsda);
-      
-      printf("generated lsda: %p \n", (void *)lsda);
-      //context->lsda = (void *) lsda;
-      
-    }
-
-}
-
 const unsigned char *
 test_extract_cie_info (const struct test_dwarf_cie *cie, struct test_Unwind_Context *context,
     test_Unwind_FrameState *fs)
@@ -778,6 +625,19 @@ test_uw_install_context_1 (struct test_Unwind_Context *current, struct test_Unwi
     return 0;
     
 }
+
+/* Frob exception handler's address kept in TARGET before installing into
+   CURRENT context.  */
+
+inline void *
+test_uw_frob_return_addr (struct test_Unwind_Context *current
+		     __attribute__ ((__unused__)),
+		     struct test_Unwind_Context *target)
+{
+  void *ret_addr = __builtin_frob_return_addr (target->ra); //TODO: Check if this is avilable to use directly: https://gcc.gnu.org/onlinedocs/gcc/Return-Address.html
+  return ret_addr;
+} 
+
 #ifdef __cplusplus
 }
 #endif
